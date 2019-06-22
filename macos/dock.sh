e95d0b48448b702f5a1ed037a8bfe4d643a9b539
Andreas Grafen <andreas@grafen.info>
2019-06-22 04:21:44 +0200

    init dotfiles

---
+++ macos/dock.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Forklift.app"
+dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
+dockutil --no-restart --add "/Applications/Microsoft Word.app"
+dockutil --no-restart --add "/Applications/CodeKit.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
+dockutil --no-restart --add "/Applications/Telegram Desktop.app"
+dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/Typeface.app"
+dockutil --no-restart --add "/Applications/Fork.app"
+dockutil --no-restart --add "/Applications/Atom.app"
+dockutil --no-restart --add "/Applications/Typeface.app"
+dockutil --no-restart --add "/Applications/Buttercup.app"
+dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
+
+killall Dock
